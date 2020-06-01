b8b1cd8b4f197de413adfaf659551a06840171fb
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-05-31 22:05:23 -0700

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,24 @@
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    // Your code goes here
+    if (string.length <= 1) return true;
+  const firstHalfEndIndex = Math.floor((string.length-2)/2);
+  const secondHalfStartIndex = Math.ceil((string.length/2));
+
+  const charStack = new Stack();
+
+  for (let i=0; i<= firstHalfEndIndex; i++) {
+    charStack.push(string[i]);
+  }
+
+  for (let i=secondHalfStartIndex; i<string.length; i++) {
+    if (string[i] !== charStack.pop()) return false;
+  }
+  return true;
+}
+
+// True, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
